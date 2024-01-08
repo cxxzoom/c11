37fd0c79fb9a4f353b61b08ba2cf5a90fd34674a
ucdo <phpsarc@gmail.com>
2024-01-08 18:16:23 +0800

    Update getput.c

--- getput.c
+++ getput.c
@@ -1,15 +1,45 @@
 #include<stdio.h>
 #include<ctype.h>
 #define SPACE ' ' // SPACE表示单引号-空格-单引号
+int test2(void);
+int test3(void);
+int test_eof(void);
 int main(void)
 {
-    char ch;
-    while ((ch = getchar()) != '\n') // 当一行未结束时
-    {
-        if (isalpha(ch)) // 留下空格
-            putchar(ch + 1); // 改变其他字符
-        else putchar(ch);
+    test_eof();
+}
+
+int test2()
+{
+    /**
+     * 该程序要记录前一个字符和当前字符。用“Receive your eieio award”这
+     *  样的输入来测试。
+    */
+   char target[] = "ei";
+   int count = 0;
+   char pre,cur;
+   cur = getchar();
+   while(cur != '#'){
+      pre = cur;
+      cur = getchar();
+      target[0] == pre && target[1] == cur && ++count;
+   }
+
+   printf("times = %d",count);
+}
+
+int test3(void){
+    char c;
+    while((c = getchar()) != '#'){
+        putchar(c);
     }
-    putchar(ch); // 打印换行符
+    return 0;
+}
+
+int test_eof(void){
+    int ch;
+    while ((ch = getchar()) != EOF)
+    putchar(ch);
+    return 0;
     return 0;
 }
